2b7860546535a0334cae37ae0fd41340fa8904b2
looksaw <xdu2814031084@gmail.com>
2026-05-21 12:10:21 +0800

    lesson3

--- internal/agent/llm/message.go
+++ internal/agent/llm/message.go
@@ -10,9 +10,19 @@ const(
 	SystemRole Role = "system"
 	//用户角色
 	UserRole Role = "user"
-	//
+	//助手的角色
 	AssitantRole Role = "assitant"
-	//
+	//Tool的角色
 	TooRole Role = "tool"
-	
 )
+
+//然后对应的消息的抽象
+type Message struct {
+	Role Role
+	Content string
+	ToolCalls []ToolCall 
+}
+//使用工具调用
+type ToolCall struct {
+
+}
